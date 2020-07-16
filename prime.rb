17fa89b5f755cec0184bdb28935bb3b2c9e42ae5
Warda Ayaz <wayaz107@gmail.com>
2020-07-16 06:31:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
 def prime?(item)
-  return false if item == 1
-
+  if item <= 1 return false
+elsif
 (2..(item - 1)).each do |number|
   if item % number == 0
    return false
  end
  end
- return true
+ end
+  true
 end
